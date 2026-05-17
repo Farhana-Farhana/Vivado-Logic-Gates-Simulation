c515a4816f79a6c963eb8d6255c55a3618f6c0df
N S Farhana <nsfarhana.2004@gmail.com>
2026-05-17 23:52:06 +0530

    Add files via upload

---
+++ not_tb.v
@@ -0,0 +1,37 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.02.2026 14:11:08
+// Design Name: 
+// Module Name: and_gate_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate_tb();
+reg a,b;
+wire Y;
+
+and_gate uut(a,b,Y);
+
+initial begin
+    a=0;b=0;
+    #10 a=0;b=1;
+    #10 a=1;b=0;
+    #10 a=1;b=1;
+    #10;
+    $finish;
+    end
+    endmodule

f98292cc542f017c24548a467bbaae714f0f3d92
N S Farhana <nsfarhana.2004@gmail.com>
2026-05-17 23:42:44 +0530

    Add files via upload

---
+++ xor_gate.v
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 28.02.2026 14:06:59
+// Design Name: 
+// Module Name: and_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module and_gate(
+    input a,b,
+    output Y
+    );
+    assign Y=a^b;
+endmodule
